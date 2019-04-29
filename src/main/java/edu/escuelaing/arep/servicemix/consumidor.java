9943892493b2bf88dbc8d73d6b79c1aecf2bbc63
estudiante <estudiante@linux-61.labinfo.is.escuelaing.edu.co>
2019-04-29 17:34:33 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arep/servicemix/consumidor.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arep.servicemix;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class consumidor implements Runnable, ExceptionListener {
+
+    public static void main(String[] args) {
+        consumidor ex = new consumidor();
+        ex.run();
+    }
+
+    public void run() {
+
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(String.format("tcp://%s:%s", "52.207.249.24", "61616?jms.useAsyncSend=true"));
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection("smx","smx");
+            connection.start();
+
+            connection.setExceptionListener(this);
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("salida");
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void onException(JMSException ex) {
+        System.out.println("JMS Exception occured.  Shutting down client.");
+    }
+}
---
+++ src/main/java/edu/escuelaing/arep/servicemix/productor.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arep.servicemix;
+
+/**
+ *
+ * @author estudiante
+ */
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class productor {
+
+    public static void main(String[] args) 
+    {
+        productor ex = new productor();
+        ex.run();
+    }
+        
+    public void run(){    
+        CamelContext context = new DefaultCamelContext();
+        //String brokerURL = args[0];
+        String hola=String.format("tcp://%s:%s", "52.207.249.24", "61616?jms.useAsyncSend=true");
+        System.out.println(hola);
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(String.format("tcp://%s:%s", "52.207.249.24", "61616?jms.useAsyncSend=true"));
+        try {
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection("smx","smx");
+                connection.start();
+ 
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+ 
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("events");
+ 
+                // Create a MessageProducer from the Session to the Topic or Queue
+                MessageProducer producer = session.createProducer(destination);
+                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+ 
+                // Create a messages
+                String text = "Hola Estoy Vivo! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+                TextMessage message = session.createTextMessage(text);
+ 
+                // Tell the producer to send the message
+                System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+                producer.send(message);
+ 
+                // Clean up
+                session.close();
+                connection.close();
+            }
+            catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+    }
+}
